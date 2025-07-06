f7a57b54b0d646397e93679fbcc5047b1295530d
Kofi Boateng <114964850+kp-debug@users.noreply.github.com>
2025-07-06 08:33:50 +0100

    Update sermons.js

--- backend/routes/sermons.js
+++ backend/routes/sermons.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Sermon = require('../models/sermon');
+const Sermon = require('../models/Sermon');
 
 const multer = require('multer');
 const path = require('path');
